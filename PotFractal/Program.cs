2cdb09796ca5f7877db661202aab1487498ff322
mpatnode1 <149105967+mpatnode1@users.noreply.github.com>
2024-01-25 10:09:10 -0600

    Add files via upload

---
+++ PotFractal/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DogonPotVariation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title="Example steps to draw a Dogon-like pot.";
+            new Pot();
+            Console.ReadKey();
+
+        }
+    }
+}
